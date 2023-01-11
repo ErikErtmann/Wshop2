f862763d508f73d3fb26cf8c3d1f21e09cc0b797
ErikErtmann <115531590+ErikErtmann@users.noreply.github.com>
2023-01-11 10:26:32 +0200

    Initial commit

---
+++ wshop2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Footer from './global/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      Tere tulemast Jämmi kodulehele
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ wshop2/src/global/Footer.jsx
@@ -0,0 +1,23 @@
+import "../css/Footer.css";
+
+function Footer() {
+    return (
+    <div className="containerfooter">
+      <div className="upper">
+        <div className="row1">test</div>
+        <div className="row2">test</div>
+        <div className="row3">test</div>
+      </div>
+      <div className="footer">
+        <div className="footerbox">
+            Copyright @ 2023 Erik Ertmann
+        </div>
+        <div className="footerbox rtl">
+            Jämmi corp
+        </div>
+      </div>
+    </div>  
+    );
+}
+
+export default Footer;
